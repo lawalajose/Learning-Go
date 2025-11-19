0c5927e3d38508cf20de8f3d818add45c39db79d
AbdulAwwal <ajoselawal@gmail.com>
2025-11-19 12:43:05 +0100

    demo on error handling

--- go-exercises/demo/errors/errors.go
+++ go-exercises/demo/errors/errors.go
@@ -1,6 +1,32 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+type Stuff struct {
+	values []int
 }
 
+func (s *Stuff) Get(index int) (int, error) {
+	if index > len(s.values) {
+		return 0, errors.New(fmt.Sprintf("no element at index %v", index))
+	} else {
+		return s.values[index], nil
+	}
+}
+
+func main() {
+	stuff := Stuff{
+		values: []int{1, 3, 4, 5},
+	}
+
+	value, err := stuff.Get(1)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Value is %v ", value)
+	}
+	fmt.Println()
+}
